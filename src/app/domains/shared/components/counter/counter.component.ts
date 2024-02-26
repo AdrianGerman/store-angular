aa36c794f7650c66f1a5df838f4d0c5b9a88d1a6
AdrianGerman <adrian.german1019@gmail.com>
2024-02-26 12:20:28 -0600

    methods ngOn

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, SimpleChange } from '@angular/core';
+import {
+  Component,
+  Input,
+  SimpleChange,
+  SimpleChanges,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -10,6 +16,8 @@ import { Component, Input, SimpleChange } from '@angular/core';
 export class CounterComponent {
   @Input({ required: true }) duration: number = 0;
   @Input({ required: true }) message: string = '';
+  counter = signal(0);
+  counterRef: number | undefined;
 
   constructor() {
     // NO ASYNC
@@ -17,10 +25,39 @@ export class CounterComponent {
     console.log('-'.repeat(10));
   }
 
-  ngOnChanges(changes: SimpleChange) {
-    // before and during render
+  ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+    const duration = changes['duration'];
+    if (duration && duration.currentValue !== duration.previousValue) {
+      this.doSomething();
+    }
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+    this.counterRef = window.setInterval(() => {
+      console.log('run interval');
+      this.counter.update((statePrev) => statePrev + 1);
+    }, 1000);
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef);
+  }
+
+  doSomething() {
+    console.log('change duration');
   }
 }

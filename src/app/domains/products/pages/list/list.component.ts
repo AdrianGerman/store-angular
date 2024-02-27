3f2fbbfdc3eb1eb4dc034c11e24bca33b09f87c7
AdrianGerman <adrian.german1019@gmail.com>
2024-02-27 09:27:49 -0600

    header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -36,6 +36,41 @@ export class ListComponent {
         image: 'https://picsum.photos/600/640?r=11',
         creationAt: new Date().toISOString(),
       },
+      {
+        id: Date.now(),
+        title: 'Samsung tab s9 fe',
+        price: 330,
+        image: 'https://picsum.photos/600/640?r=34',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 7',
+        price: 100,
+        image: 'https://picsum.photos/600/640?r=43',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pad 6',
+        price: 120,
+        image: 'https://picsum.photos/600/640?r=32',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pad 6s',
+        price: 130,
+        image: 'https://picsum.photos/600/640?r=61',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Samsung tab s9 fe',
+        price: 330,
+        image: 'https://picsum.photos/600/640?r=65',
+        creationAt: new Date().toISOString(),
+      },
     ];
     this.products.set(initProducts);
   }

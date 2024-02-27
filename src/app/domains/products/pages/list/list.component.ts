2097965f4a9830facd92266b36f3e62647870f02
AdrianGerman <adrian.german1019@gmail.com>
2024-02-27 08:34:18 -0600

    products component

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,35 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 7',
+        price: 100,
+        image: 'https://picsum.photos/600/640?r=23',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pad 6',
+        price: 120,
+        image: 'https://picsum.photos/600/640?r=77',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pad 6s',
+        price: 130,
+        image: 'https://picsum.photos/600/640?r=11',
+        creationAt: new Date().toISOString(),
+      },
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string) {
     console.log('estamos en el padre');
     console.log(event);

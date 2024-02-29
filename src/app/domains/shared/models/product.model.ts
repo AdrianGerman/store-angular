911ec6c68dfbf6625d9e303fc76ab01418299117
AdrianGerman <adrian.german1019@gmail.com>
2024-02-29 12:48:26 -0600

    image gallery

--- src/app/domains/shared/models/product.model.ts
+++ src/app/domains/shared/models/product.model.ts
@@ -1,9 +1,11 @@
+import { Category } from './category.model';
+
 export interface Product {
   id: number;
   title: string;
   price: number;
   description: string;
-  categoryId: number;
+  category: Category;
   images: string[];
   creationAt: string;
 }
